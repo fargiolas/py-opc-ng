8f798ebb29eae92dcefeed72b661a9e02b55ed6a
Filippo Argiolas <filippo.argiolas@gmail.com>
2020-11-04 05:41:38 +0100

    n3: data map logic for PM and Power State
    Use the data structure logic from the previous commit also for the other
    data reading functions. Still not sure about how to handle the data
    types, a dedicated class seems overkill. All this stuff should make it
    pretty easy to support different devices (R1, N2?): given each one
    returns different data types and structures with the same opcodes it
    should be possible to just change the data map and keep some shared
    code.

--- opcn3.py
+++ opcn3.py
@@ -4,21 +4,21 @@ from time import sleep
 from pprint import pprint
 
 def _unpack(t, x):
-    if t == "uchar":
+    if t == 'uchar':
         return x[0]
-    elif t == "ushort":
+    elif t == 'ushort':
         return (x[1] << 8) | x[0]
-    elif t == "float":
+    elif t == 'float':
         return struct.unpack('f', struct.pack('4B', *x))[0]
     else:
         raise ValueError
 
 def _len(t):
-    if t == "uchar":
+    if t == 'uchar':
         return 1
-    elif t == "ushort":
+    elif t == 'ushort':
         return 2
-    elif t == "float":
+    elif t == 'float':
         return 4
     else:
         raise ValueError
@@ -40,67 +40,80 @@ OPC_N3_POWER_OPTION_LASER_POT    = 2
 OPC_N3_POWER_OPTION_LASER_SWITCH = 3
 OPC_N3_POWER_OPTION_GAIN_TOGGLE  = 4
 
-OPC_N3_HISTOGRAM_MAP = [
-    ['Bin 0',              'ushort'],
-    ['Bin 1',              'ushort'],
-    ['Bin 2',              'ushort'],
-    ['Bin 3',              'ushort'],
-    ['Bin 4',              'ushort'],
-    ['Bin 5',              'ushort'],
-    ['Bin 6',              'ushort'],
-    ['Bin 7',              'ushort'],
-    ['Bin 8',              'ushort'],
-    ['Bin 9',              'ushort'],
-    ['Bin 10',             'ushort'],
-    ['Bin 11',             'ushort'],
-    ['Bin 12',             'ushort'],
-    ['Bin 13',             'ushort'],
-    ['Bin 14',             'ushort'],
-    ['Bin 15',             'ushort'],
-    ['Bin 16',             'ushort'],
-    ['Bin 17',             'ushort'],
-    ['Bin 18',             'ushort'],
-    ['Bin 19',             'ushort'],
-    ['Bin 20',             'ushort'],
-    ['Bin 21',             'ushort'],
-    ['Bin 22',             'ushort'],
-    ['Bin 23',             'ushort'],
-    ['Bin1 MToF',          'uchar'],
-    ['Bin3 MToF',          'uchar'],
-    ['Bin5 MToF',          'uchar'],
-    ['Bin7 MToF',          'uchar'],
-    ['Sampling Period',    'ushort'],
-    ['SFR',                'ushort'],
-    ['Temperature',        'ushort'],
-    ['Relative humidity',  'ushort'],
-    ['PM1',                'float'],
-    ['PM2.5',              'float'],
-    ['PM10',               'float'],
-    ['#RejectGlitch',      'ushort'],
-    ['#RejectLongTOF',     'ushort'],
-    ['#RejectRatio',       'ushort'],
-    ['#RejectOutOfRange',  'ushort'],
-    ['Fan rev count',      'ushort'],
-    ['Laser status',       'ushort'],
-    ['Checksum',           'ushort']]
+OPC_N3_POWER_OPTION_MAP = [['FanON',              'uchar'],
+                           ['LaserON',            'uchar'],
+                           ['FanDACVal',          'uchar'],
+                           ['LaserDACVal',        'uchar'],
+                           ['LaserSwitch',        'uchar'],
+                           ['GainToggle',         'uchar']]
 
-def _histogram_len(h):
-    l = 0
-    for k, t in h:
-        l += t.len()
+OPC_N3_PM_MAP =           [['PM1',                'float'],
+                           ['PM2.5',              'float'],
+                           ['PM10',               'float'],
+                           ['Checksum',          'ushort']]
 
-    return l
+OPC_N3_HISTOGRAM_MAP    = [['Bin 0',             'ushort'],
+                           ['Bin 1',             'ushort'],
+                           ['Bin 2',             'ushort'],
+                           ['Bin 3',             'ushort'],
+                           ['Bin 4',             'ushort'],
+                           ['Bin 5',             'ushort'],
+                           ['Bin 6',             'ushort'],
+                           ['Bin 7',             'ushort'],
+                           ['Bin 8',             'ushort'],
+                           ['Bin 9',             'ushort'],
+                           ['Bin 10',            'ushort'],
+                           ['Bin 11',            'ushort'],
+                           ['Bin 12',            'ushort'],
+                           ['Bin 13',            'ushort'],
+                           ['Bin 14',            'ushort'],
+                           ['Bin 15',            'ushort'],
+                           ['Bin 16',            'ushort'],
+                           ['Bin 17',            'ushort'],
+                           ['Bin 18',            'ushort'],
+                           ['Bin 19',            'ushort'],
+                           ['Bin 20',            'ushort'],
+                           ['Bin 21',            'ushort'],
+                           ['Bin 22',            'ushort'],
+                           ['Bin 23',            'ushort'],
+                           ['Bin1 MToF',          'uchar'],
+                           ['Bin3 MToF',          'uchar'],
+                           ['Bin5 MToF',          'uchar'],
+                           ['Bin7 MToF',          'uchar'],
+                           ['Sampling Period',   'ushort'],
+                           ['SFR',               'ushort'],
+                           ['Temperature',       'ushort'],
+                           ['Relative humidity', 'ushort'],
+                           ['PM1',                'float'],
+                           ['PM2.5',              'float'],
+                           ['PM10',               'float'],
+                           ['#RejectGlitch',     'ushort'],
+                           ['#RejectLongTOF',    'ushort'],
+                           ['#RejectRatio',      'ushort'],
+                           ['#RejectOutOfRange', 'ushort'],
+                           ['Fan rev count',     'ushort'],
+                           ['Laser status',      'ushort'],
+                           ['Checksum',          'ushort']]
+
+
+def _map_size(m):
+        l = 0
+        for k, t in m:
+            l += _len(t)
+
+        return l
 
 
 class OPC(object):
     def __init__(self, spi):
         self.spi = spi
-        self._histogram_length = None
-        self.pm_length = None
+        self.histogram_map_size = _map_size(OPC_N3_HISTOGRAM_MAP)
+        self.poweroption_map_size = _map_size(OPC_N3_POWER_OPTION_MAP)
+        self.pm_map_size = _map_size(OPC_N3_PM_MAP)
 
     def _send_command(self, cmd):
         r = self.spi.xfer([cmd])[0]
-        # print("cmd: 0x{:02X} r: 0x{:02X}".format(cmd, r))
+        # print('cmd: 0x{:02X} r: 0x{:02X}'.format(cmd, r))
         sleep(0.02)
         return r
 
@@ -111,10 +124,10 @@ class OPC(object):
         while (r != OPC_READY):
             if attempts >= 20:
                 if attempts >= 20+5:
-                    print("something wrong, aborting")
+                    print('something wrong, aborting')
                     return None
 
-                print("opc not responding, let's wait...")
+                print('opc not responding, let\'s wait...')
                 sleep(3)  # > 2s ( < 10s)
 
             r = self._send_command(cmd)
@@ -123,19 +136,9 @@ class OPC(object):
 
         return r
 
-    def histogram_length(self):
-        if self._histogram_length == None:
-            l = 0
-            for k, t in OPC_N3_HISTOGRAM_MAP:
-                l += _len(t)
-
-            self._histogram_length = l
-
-        return self._histogram_length
-
     def info(self):
         self._wait_for_command(OPC_CMD_READ_INFO_STRING)
-        info = ""
+        info = ''
         for i in range(60):
             info += chr(self._send_command(OPC_CMD_READ_INFO_STRING))
 
@@ -143,7 +146,7 @@ class OPC(object):
 
     def serial(self):
         self._wait_for_command(OPC_CMD_READ_SERIAL_STRING)
-        info = ""
+        info = ''
         for i in range(60):
             info += chr(self._send_command(OPC_CMD_READ_SERIAL_STRING))
 
@@ -190,7 +193,7 @@ class OPC(object):
 
     def power_status(self):
         self._wait_for_command(OPC_CMD_READ_POWER_STATE)
-        keys = ["FanON", "LaserON", "FanDACVal", "LaserDACVal", "LaserSwitch", "GainToggle"]
+        keys = ['FanON', 'LaserON', 'FanDACVal', 'LaserDACVal', 'LaserSwitch', 'GainToggle']
         r = dict()
         for k in keys:
             r[k] = self._send_command(OPC_CMD_READ_POWER_STATE)
@@ -216,12 +219,10 @@ class OPC(object):
 
     def histogram(self):
         self._wait_for_command(OPC_CMD_READ_HISTOGRAM)
-        length = self.histogram_length()
         raw_bytes = []
-        for i in range(length):
+        for i in range(self.histogram_map_size):
             raw_bytes += [self._send_command(OPC_CMD_READ_HISTOGRAM)]
         print(raw_bytes)
-        print(length)
 
         hist = dict()
         c = 0
@@ -231,33 +232,54 @@ class OPC(object):
             hist[k] = _unpack(t, raw)
             c += l
 
-        if "Checksum" in hist.keys():
+        if 'Checksum' in hist.keys():
             crc = self.checksum(raw_bytes[:-2])
-            if hist["Checksum"] != crc:
-                print("WRONG CHECKSUM")
+            if hist['Checksum'] != crc:
+                print('WRONG CHECKSUM')
                 return None
 
         return self.histogram_post_process(hist)
 
     def histogram_post_process(self, hist):
-        hist["Temperature"] = -45. + 175. * hist["Temperature"] / (float(1<<16) - 1.)
-        hist["Relative humidity"] = 100. * hist["Relative humidity"] / (float(1<<16) - 1.)
-        hist["Sampling Period"] = hist["Sampling Period"] / 100.
-        hist["SFR"] = hist["SFR"] / 100.
+        hist['Temperature'] = -45. + 175. * hist['Temperature'] / (float(1<<16) - 1.)
+        hist['Relative humidity'] = 100. * hist['Relative humidity'] / (float(1<<16) - 1.)
+        hist['Sampling Period'] = hist['Sampling Period'] / 100.
+        hist['SFR'] = hist['SFR'] / 100.
 
-        ml_per_bin = hist["SFR"] * hist["Sampling Period"]
+        ml_per_bin = hist['SFR'] * hist['Sampling Period']
         for k, t in OPC_N3_HISTOGRAM_MAP:
-            if "Bin " in k:
+            if 'Bin ' in k:
                 hist[k] = hist[k] / ml_per_bin
 
         for k, t in OPC_N3_HISTOGRAM_MAP:
-            if "MToF" in k:
+            if 'MToF' in k:
                 hist[k] = hist[k] / 3.
 
         for k, t in OPC_N3_HISTOGRAM_MAP:
-            print("{}: {}".format(k, hist[k]))
+            print('{}: {}'.format(k, hist[k]))
 
-    # def pm(self):
+    def pm(self):
+        self._wait_for_command(OPC_CMD_READ_PM)
+        raw_bytes = []
+        for i in range(self.pm_map_size):
+            raw_bytes += [self._send_command(OPC_CMD_READ_PM)]
+        print(raw_bytes)
+
+        pm = dict()
+        c = 0
+        for k, t in OPC_N3_PM_MAP:
+            l = _len(t)
+            raw = raw_bytes[c:c+l]
+            pm[k] = _unpack(t, raw)
+            c += l
+
+        if 'Checksum' in pm.keys():
+            crc = self.checksum(raw_bytes[:-2])
+            if pm['Checksum'] != crc:
+                print('WRONG CHECKSUM')
+                return None
+
+        return pm
     #     self._wait_for_command(OPC_CMD_READ_PM)
     #     pma = self._read_float(OPC_CMD_READ_PM)
     #     pmb = self._read_float(OPC_CMD_READ_PM)
@@ -267,26 +289,28 @@ class OPC(object):
     #     print(pma, pmb, pmc, hex(chksum))
 
 
-if __name__ == "__main__":
-    spi = SPI("/dev/ttyACM0")
+if __name__ == '__main__':
+    spi = SPI('/dev/ttyACM0')
     spi.mode = 1
     spi_max_speed_hz = 500000
 
     opc = OPC(spi)
 
-    print("ping?")
-    print("pong!" if opc.ping() else "timeout!")
-    print("info:      {}".format(opc.info()))
-    print("serial:    {}".format(opc.serial()))
-    print("fwversion: {}.{}".format(*opc.fwversion()))
-    print("on")
+    print('ping?')
+    print('pong!' if opc.ping() else 'timeout!')
+    print('info:      {}'.format(opc.info()))
+    print('serial:    {}'.format(opc.serial()))
+    print('fwversion: {}.{}'.format(*opc.fwversion()))
+    print('on')
     opc.on()
     pprint(opc.power_status())
     sleep(3)
     for i in range(5):
         opc.histogram()
         sleep(1)
-    print("off")
+        pprint(opc.pm())
+        sleep(1)
+    print('off')
     opc.off()
     pprint(opc.power_status())
 
@@ -295,10 +319,10 @@ if __name__ == "__main__":
 
     # sleep(1)
 
-    # print("dac and power status")
+    # print('dac and power status')
 
     # wait_for_command(OPC_CMD_READ_POWER_STATE)
-    # keys = ["FanON", "LaserON", "FanDACVal", "LaserDACVal", "LaserSwitch", "GainToggle"]
+    # keys = ['FanON', 'LaserON', 'FanDACVal', 'LaserDACVal', 'LaserSwitch', 'GainToggle']
     # r = dict()
     # for k in keys:
     #     r[k] = send_command(OPC_CMD_READ_POWER_STATE)
@@ -310,10 +334,10 @@ if __name__ == "__main__":
 
     # sleep(1)
 
-    # print("dac and power status")
+    # print('dac and power status')
 
     # wait_for_command(OPC_CMD_READ_POWER_STATE)
-    # keys = ["FanON", "LaserON", "FanDACVal", "LaserDACVal", "LaserSwitch", "GainToggle"]
+    # keys = ['FanON', 'LaserON', 'FanDACVal', 'LaserDACVal', 'LaserSwitch', 'GainToggle']
     # r = dict()
     # for k in keys:
     #     r[k] = send_command(OPC_CMD_READ_POWER_STATE)
