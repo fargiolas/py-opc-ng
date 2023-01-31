a0be3035dc335f4b114b11252d306170a0ec852c
Filippo Argiolas <filippo.argiolas@gmail.com>
2023-01-31 09:27:22 +0100

    bump docs release

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2022, Filippo Argiolas'
 author = 'Filippo Argiolas'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.1'
+release = '0.0.5'
 
 master_doc = 'index'
 
